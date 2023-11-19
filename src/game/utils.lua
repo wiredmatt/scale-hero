9554548293bbed2bb2d6634216d99789146a2fb3
wiredmatt <29718978+wiredmatt@users.noreply.github.com>
2023-11-19 00:37:35 -0300

    chore: cleanup utils

--- src/game/utils.lua
+++ src/game/utils.lua
@@ -1,33 +1,4 @@
-local Atlas = require "src.tool.atlas"
-
 local utils = {}
-
-function utils:getScreenCenter()
-  local cx, cy = _G.WIDTH, _G.HEIGHT
-
-  self.cx = cx
-  self.cy = cy
-
-  return cx, cy
-end
-
-function utils:getScreen00()
-  local cw, ch = self:getScreenCenter()
-
-  local zx, zy = cw / 2, ch / 2
-
-  self.zx = zx
-  self.zy = zy
-
-  return zx, zy
-end
-
-function utils:getSpriteCenter(sname)
-  local _, quad = Atlas.lib.getSprite(sname)
-
-  local _, _, w, h = quad:getViewport()
-  return w / 2, h / 2
-end
 
 ---@param active_region_quad love.Quad
 ---@param x number
@@ -47,13 +18,5 @@ function utils.isInQuad(active_region_quad, x, y, w, h)
       b.x + b.w <= a.x + a.w and
       b.y + b.h <= a.y + a.h
 end
-
-setmetatable(utils, {
-  __call = function(self)
-    self:getScreenCenter()
-    self:getScreen00()
-    return self
-  end
-})
 
 return utils
